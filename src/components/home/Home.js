127c1e0f71d33f3e00f057c2d00765d18f4ad513
Akshay Kumar <akshay.kumar@sdiot.io>
2020-08-20 18:39:42 +0530

    Update Home component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='w-full flex flex-col p-10'>
+      <div className='text-5xl'>Tell your story <br />the right way</div>
+      <div className='w-full max-w-sm'>Nisi nulla ut exercitation eu quis ex cillum et id. Id ea cupidatat elit commodo. Non eu qui nisi cillum nulla. Ad commodo do nostrud do. Duis Lorem ad ex ullamco id commodo culpa voluptate sunt non magna. Et enim dolor qui incididunt nulla voluptate nulla irure minim eu. Sint minim ad do non cupidatat tempor eu eu ea et qui.</div>
+      <div className='pt-10'>
+        <button className='bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full'>Book your consultation now</button>
+      </div>
     </div>
   );
 };
