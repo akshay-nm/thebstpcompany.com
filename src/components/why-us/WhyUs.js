253240b9ed5e8578ca856023e5b5b28b5724fe66
Akshay Kumar <akshay.kumar@sdiot.io>
2020-08-20 18:40:08 +0530

    Update WhyUs component

--- src/components/why-us/WhyUs.js
+++ src/components/why-us/WhyUs.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 const WhyUs = () => {
   return (
-    <div>
-      WhyUs
+    <div className='p-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 bg-gray-900 text-white'>
+      <div className='flex flex-col items-center text-center p-4'>
+        <div className='border rounded-full bg-gray-500 p-16'></div>
+        <div className='text-xs w-full max-w-xs mt-4'>Id tempor anim elit ea fugiat consectetur incididunt voluptate ea. Aliquip fugiat anim cillum tempor.</div>
+      </div>
+      <div className='flex flex-col items-center text-center p-4'>
+        <div className='border rounded-full bg-gray-500 p-16'></div>
+        <div className='text-xs w-full max-w-xs mt-4'>Labore consectetur sit ipsum voluptate. Fugiat occaecat esse irure anim voluptate.</div>
+      </div>
+      <div className='flex flex-col items-center text-center p-4'>
+        <div className='border rounded-full bg-gray-500 p-16'></div>
+        <div className='text-xs w-full max-w-xs mt-4'>Laboris non voluptate mollit incididunt aute labore laborum in minim. Magna qui aute voluptate id deserunt tempor duis elit consequat Lorem culpa ex.</div>
+      </div>
+      <div className='flex flex-col items-center text-center p-4'>
+        <div className='border rounded-full bg-gray-500 p-16'></div>
+        <div className='text-xs w-full max-w-xs mt-4'>Et aliqua dolore ut officia aliquip deserunt voluptate magna proident esse Lorem qui sunt incididunt. Aliquip elit adipisicing proident fugiat culpa dolore fugiat minim dolor sint adipisicing qui elit.</div>
+      </div>
     </div>
   );
 };
