87c7727ab1db1fa19b3b3f43cefa2beb622d6e3d
Akshay Kumar <akshay.kumar@sdiot.io>
2020-08-21 05:33:31 +0530

    Make it responsive

--- src/components/contact-us/ContactUs.js
+++ src/components/contact-us/ContactUs.js
@@ -2,8 +2,33 @@ import React from 'react';
 
 const ContactUs = () => {
   return (
-    <div>
-      Contact Us
+    <div className='bg-gray-900 text-white'>
+      <div className='text-center text-5xl'>Contact Us</div>
+      <div className='grid grid-cols-1 md:grid-cols-2 p-10 gap-20'>
+        <div>
+          <input className='border-b-4 mb-3 bg-gray-900 w-full' placeholder='Name' type='text' id='name' />
+          <input className='border-b-4 mb-3 bg-gray-900 w-full' placeholder='E-mail' type='text' id='name' />
+          <input className='border-b-4 mb-3 bg-gray-900 w-full' placeholder='Phone' type='text' id='name' />
+          <div className='mb-3'>Your Inquiry</div>
+          <input className='border-b-4 mb-3 bg-gray-900 w-full' placeholder='' type='text' id='name' />
+          <div className='text-right'>
+            <button className='bg-white text-black py-1 px-2 rounded'>Submit</button>
+          </div>
+        </div>
+        <div>
+          <div className='text-sm'>Write to us</div>
+          <div className='font-black text-lg sm:text-xl md:text-2xl'>shuja@thebstpcompany.com</div>
+          <div>
+            <div className='text-sm'>Follow us</div>
+            <div className='flex'>
+              <div className='rounded-full p-4 m-2 bg-gray-500'></div>
+              <div className='rounded-full p-4 m-2 bg-gray-500'></div>
+              <div className='rounded-full p-4 m-2 bg-gray-500'></div>
+            </div>
+          </div>
+          <div className='text-xs text-justify p-4 mx-10'>Copyright 2020 @Haya Abidi | 221b Baker Street | Suite 1106 | Blackpink in your area | 999-096-3975</div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navigation = () => {
+  const [expanded, set] = useState(false)
   return (
-    <div>
-      Navigation
-    </div>
+    <nav className='w-full flex items-center justify-between p-10'>
+      <div>
+        <div>Logo</div>
+        <div className={`block ${expanded? '' : 'hidden'} sm:hidden`}>
+          <div>Why Us</div>
+          <div>Pricing</div>
+          <div>Contact Us</div>
+        </div>
+      </div>
+      <div className="block sm:hidden">
+        <button className="flex items-center px-3 py-2 border rounded text-gray-900 border-gray-400 hover:text-gray-700 hover:border-white" onClick={() => set(!expanded)}>
+          <div className={`${expanded? 'hidden': ''}`}><svg className='fill-current h-3 w-3' viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg></div>
+          <div className={`${expanded? '': 'hidden'}`}>x</div>
+        </button>
+      </div>
+      <div className='hidden sm:flex flex-row-reverse'>
+        <div className='ml-10'>Contact Us</div>
+        <div className='ml-10'>Pricing</div>
+        <div className='ml-10'>Why Us</div>
+      </div>
+    </nav>
   );
 };
 
