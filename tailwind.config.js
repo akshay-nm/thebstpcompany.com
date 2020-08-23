286c97c668e2d89465ad8cfea56299706a5e45f6
Akshay Kumar <akshay.kumar@sdiot.io>
2020-08-23 19:21:33 +0530

    Remove empty keys in theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,6 @@ module.exports = {
         'secondary': '#001524',
         'button-primary': '#244D58',
         'button-secondary': '#F9EFE4',
-        'text-primary': '',
-        'text-secondary': '',
       },
       fontFamily: {
         'heading': ['Abril Fatface', 'cursive'],
