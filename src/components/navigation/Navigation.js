9ab89295dc53c3e6ff0e5e83972e9dcc113dc7ce
Akshay Kumar <akshay.kumar@sdiot.io>
2020-08-26 13:34:46 +0530

    Fix font colors, adjust cats

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -5,23 +5,23 @@ const Navigation = ({ changeSection }) => {
   return (
     <nav className='w-full flex items-center justify-between px-10 py-5 fixed bg-primary'>
       <div>
-        <button onClick={() => changeSection(1)} className='text-3xl tracking-wider focus:outline-none'>The BSTP Company</button>
+        <button onClick={() => changeSection(1)} className='text-3xl tracking-wider focus:outline-none text-button-primary'>The BSTP Company</button>
         <div className={`block ${expanded? '' : 'hidden'} sm:hidden`}>
-          <button onClick={() => changeSection(2)} className='focus:outline-none' >Why Us</button>
-          <button onClick={() => changeSection(3)} className='focus:outline-none' >Pricing</button>
-          <button onClick={() => changeSection(4)} className='focus:outline-none' >Contact Us</button>
+          <button onClick={() => changeSection(2)} className='text-button-primary focus:outline-none' >Why Us</button>
+          <button onClick={() => changeSection(3)} className='text-button-primary focus:outline-none' >Pricing</button>
+          <button onClick={() => changeSection(4)} className='text-button-primary focus:outline-none' >Contact Us</button>
         </div>
       </div>
       <div className="block sm:hidden">
-        <button className="flex items-center px-3 py-2 border rounded text-gray-900 border-gray-400 hover:text-gray-700 hover:border-white focus:outline-none" onClick={() => set(!expanded)}>
+        <button className="flex items-center px-3 py-2 border rounded text-button-primary focus:outline-none" onClick={() => set(!expanded)}>
           <div className={`${expanded? 'hidden': ''}`}><svg className='fill-current h-3 w-3' viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg></div>
           <div className={`${expanded? '': 'hidden'}`}>&#10005;</div>
         </button>
       </div>
       <div className='hidden sm:flex flex-row-reverse'>
-        <button onClick={() => changeSection(4)} className='ml-10 p-2 focus:outline-none'>Contact Us</button>
-        <button onClick={() => changeSection(3)} className='ml-10 p-2 focus:outline-none'>Pricing</button>
-        <button onClick={() => changeSection(2)} className='ml-10 p-2 focus:outline-none'>Why Us</button>
+        <button onClick={() => changeSection(4)} className='ml-10 p-2 text-button-primary focus:outline-none'>Contact Us</button>
+        <button onClick={() => changeSection(3)} className='ml-10 p-2 text-button-primary focus:outline-none'>Pricing</button>
+        <button onClick={() => changeSection(2)} className='ml-10 p-2 text-button-primary focus:outline-none'>Why Us</button>
       </div>
     </nav>
   );
--- src/components/pricing/Pricing.js
+++ src/components/pricing/Pricing.js
@@ -7,14 +7,14 @@ import lgCat from './lg-cat.svg'
 const Pricing = () => {
   return (
     <div className='px-16 py-32 bg-primary'>
-      <div className='text-center text-5xl mb-5 font-heading'>Pricing</div>
+      <div className='text-center text-5xl mb-5 font-heading text-button-primary'>Pricing</div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 p-5'>
         <div className='flex items-center justify-center'>
-          <div className='flex flex-col bg-secondary rounded-lg p-1 w-full max-w-xs'>
-            <div className='h-32'>
-              <img src={smCat} className='object-scale-down h-full w-full' alt='cat' />
+          <div className='flex flex-col bg-button-primary rounded-lg p-1 w-full max-w-xs'>
+            <div className='h-32 bg-button-primary overflow-hidden'>
+              <img src={smCat} className='h-full w-full mt-10' alt='cat' />
             </div>
-            <div className='text-center rounded-lg bg-primary p-4'>
+            <div className='text-center rounded-lg bg-primary p-4 text-button-primary'>
               <div className='text-2xl font-heading'>Small</div>
               <div>Articles per Month - 4</div>
               <div>Articles per Week - 1</div>
@@ -24,17 +24,17 @@ const Pricing = () => {
               <div>Plagarism Free - Yes</div>
               <div>Posting Included - No</div>
               <div className='text-lg'>$39.00 / month</div>
-              <button className='bg-secondary hover:bg-gray-700 text-primary font-bold py-2 px-4 mt-4 rounded-full'>Buy Now</button>
+              <button className='bg-button-primary hover:bg-gray-700 text-primary font-bold py-2 px-4 mt-4 rounded-full'>Buy Now</button>
             </div>
           </div>
         </div>
         
         <div className='flex items-center justify-center'>
-          <div className='flex flex-col bg-secondary rounded-lg p-1 w-full max-w-xs'>
-            <div className='h-32'>
-              <img src={mdCat} className='object-scale-down h-full w-full' alt='cats' />
+          <div className='flex flex-col bg-button-primary rounded-lg p-1 w-full max-w-xs'>
+            <div className='h-32 bg-button-primary overflow-hidden'>
+              <img src={mdCat} className='h-full w-full mt-10' alt='cats' />
             </div>
-            <div className='text-center rounded-lg bg-primary p-4'>
+            <div className='text-center rounded-lg bg-primary p-4 text-button-primary'>
               <div className='text-2xl font-heading'>Medium</div>
               <div>Articles per Month - 4</div>
               <div>Articles per Week - 1</div>
@@ -44,16 +44,16 @@ const Pricing = () => {
               <div>Plagarism Free - Yes</div>
               <div>Posting Included - No</div>
               <div className='text-lg'>$39.00 / month</div>
-              <button className='bg-secondary hover:bg-gray-700 text-primary font-bold py-2 px-4 mt-4 rounded-full'>Buy Now</button>
+              <button className='bg-button-primary hover:bg-gray-700 text-primary font-bold py-2 px-4 mt-4 rounded-full'>Buy Now</button>
             </div>
           </div>
         </div>
         <div className='flex items-center justify-center'>
-          <div className='flex flex-col bg-secondary rounded-lg p-1 w-full max-w-xs'>
-            <div className='h-32'>
-              <img src={lgCat} className='object-scale-down h-full w-full' alt='too many cats' />
+          <div className='flex flex-col bg-button-primary rounded-lg p-1 w-full max-w-xs'>
+            <div className='h-32 bg-button-primary overflow-hidden'>
+              <img src={lgCat} className='h-full w-full mt-10' alt='too many cats' />
             </div>
-            <div className='text-center rounded-lg bg-primary p-4'>
+            <div className='text-center rounded-lg bg-primary p-4 text-button-primary'>
               <div className='text-2xl font-heading'>Large</div>
               <div>Articles per Month - 4</div>
               <div>Articles per Week - 1</div>
@@ -63,7 +63,7 @@ const Pricing = () => {
               <div>Plagarism Free - Yes</div>
               <div>Posting Included - No</div>
               <div className='text-lg'>$39.00 / month</div>
-              <button className='bg-secondary hover:bg-gray-700 text-primary font-bold py-2 px-4 mt-4 rounded-full'>Buy Now</button>
+              <button className='bg-button-primary hover:bg-gray-700 text-primary font-bold py-2 px-4 mt-4 rounded-full'>Buy Now</button>
             </div>
           </div>
         </div>
