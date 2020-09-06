14f41654c0e2a8025f27602fc7baaa2300b2c1f2
akshay-nm <akshay.kumar@sdiot.io>
2020-09-06 18:01:30 +0530

    Add content

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import image from './3.svg';
+import React from 'react'
+import image from './3.svg'
 
 const Home = () => {
   return (
     <div className='w-full flex flex-col p-10 mt-40 relative overflow-hidden bg-primary'>
-      <div className='text-5xl md:text-6xl lg:text-hero font-heading text-button-primary '>Tell your story <br />the right way</div>
-      <div className='w-full max-w-sm md:max-w-md'>Nisi nulla ut exercitation eu quis ex cillum et id. Id ea cupidatat elit commodo. Non eu qui nisi cillum nulla. Ad commodo do nostrud do. Duis Lorem ad ex ullamco id commodo culpa voluptate sunt non magna. Et enim dolor qui incididunt nulla voluptate nulla irure minim eu. Sint minim ad do non cupidatat tempor eu eu ea et qui.</div>
+      <div className='text-5xl md:text-6xl lg:text-hero font-heading text-button-primary '>
+        Tell your story <br />
+        the right way
+      </div>
+      <div className='w-full max-w-sm md:max-w-md'>
+        People forget what they read, but they never forget how they feel
+        reading something. We help you instill the right feelings in your
+        audience. With our articles, we help you cut through the noise and drive
+        results. We focus on doing what we do best (writing articles) so you can
+        focus on what you do best (growing your business)!
+      </div>
       <div className='pt-10'>
-        <button className='bg-button-primary hover:bg-button-dark text-primary lg:text-home-button font-bold py-2 px-4 rounded-full'>Book your consultation now</button>
+        <button className='bg-button-primary hover:bg-button-dark text-primary lg:text-home-button font-bold py-2 px-4 rounded-full'>
+          Book your consultation now
+        </button>
       </div>
       <div className='absolute h-full w-1/2 bottom-0 right-0 hidden md:block'>
-      <img className='absolute h-full w-full object-contain object-right-bottom mt-5' src={image} alt='main' />
+        <img
+          className='absolute h-full w-full object-contain object-right-bottom mt-5'
+          src={image}
+          alt='main'
+        />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
--- src/components/pricing/Pricing.js
+++ src/components/pricing/Pricing.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from 'react'
 import smCat from './sm-cat.svg'
 import mdCat from './md-cat.svg'
 import lgCat from './lg-cat.svg'
 
-
 const Pricing = () => {
   return (
     <div className='px-16 py-32 bg-primary'>
-      <div className='text-center text-5xl mb-5 font-heading text-button-primary lg:text-pricing-header'>Pricing</div>
+      <div className='text-center text-5xl mb-5 font-heading text-button-primary lg:text-pricing-header'>
+        Pricing
+      </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 p-5 text-pricing-content'>
         <div className='flex items-center justify-center'>
           <div className='flex flex-col bg-button-primary rounded-lg p-1 w-full max-w-xs'>
@@ -15,61 +16,70 @@ const Pricing = () => {
               <img src={smCat} className='h-full w-full mt-10' alt='cat' />
             </div>
             <div className='text-center rounded-lg bg-primary p-4 text-button-primary'>
-              <div className='text-2xl font-heading text-pricing-sub-header'>Small</div>
-              <div>Articles per Month - 4</div>
-              <div>Articles per Week - 1</div>
-              <div>Words per article - 300</div>
-              <div>SEO Keyword Writing - Yes</div>
-              <div>Content Stategy Included - Yes</div>
-              <div>Plagarism Free - Yes</div>
-              <div>Posting Included - No</div>
-              <div className='text-pricing-content-bold'>$39.00 / month</div>
-              <button className='bg-button-primary hover:bg-gray-700 text-primary text-pricing-content-bold font-bold py-2 px-4 mt-4 rounded-full'>Buy Now</button>
+              <div className='text-2xl font-heading text-pricing-sub-header'>
+                Personal
+              </div>
+              <div>1 Pillar Page (4000 Words Each)</div>
+              <div>2 SEO Articles (2000 Words Each)</div>
+              <div>3 Custom Banners</div>
+              <div>3 Social Media Posts</div>
+              <div>Customized Content Strategy</div>
+              <div>Dedicated Account Manager</div>
+              <div className='text-pricing-content-bold'>$1000 / month</div>
+              <button className='bg-button-primary hover:bg-gray-700 text-primary text-pricing-content-bold font-bold py-2 px-4 mt-4 rounded-full'>
+                Buy Now
+              </button>
             </div>
           </div>
         </div>
-        
+
         <div className='flex items-center justify-center'>
           <div className='flex flex-col bg-button-primary rounded-lg p-1 w-full max-w-xs'>
             <div className='h-32 bg-button-primary overflow-hidden'>
               <img src={mdCat} className='h-full w-full mt-10' alt='cats' />
             </div>
             <div className='text-center rounded-lg bg-primary p-4 text-button-primary'>
-              <div className='text-2xl font-heading text-pricing-sub-header'>Medium</div>
-              <div>Articles per Month - 4</div>
-              <div>Articles per Week - 1</div>
-              <div>Words per article - 300</div>
-              <div>SEO Keyword Writing - Yes</div>
-              <div>Content Stategy Included - Yes</div>
-              <div>Plagarism Free - Yes</div>
-              <div>Posting Included - No</div>
-              <div className='text-pricing-content-bold'>$39.00 / month</div>
-              <button className='bg-button-primary hover:bg-gray-700 text-primary text-pricing-content-bold font-bold py-2 px-4 mt-4 rounded-full'>Buy Now</button>
+              <div className='text-2xl font-heading text-pricing-sub-header'>
+                Startup
+              </div>
+              <div>3 Pillar Pages (4000 Words Each)</div>
+              <div>6 SEO Articles (2000 Words Each)</div>
+              <div>9 Custom Banners</div>
+              <div>Customized Content Strategy </div>
+              <div>Dedicated Account Manager</div>
+              <div className='text-pricing-content-bold'>$2500 / month</div>
+              <button className='bg-button-primary hover:bg-gray-700 text-primary text-pricing-content-bold font-bold py-2 px-4 mt-4 rounded-full'>
+                Buy Now
+              </button>
             </div>
           </div>
         </div>
         <div className='flex items-center justify-center'>
           <div className='flex flex-col bg-button-primary rounded-lg p-1 w-full max-w-xs'>
             <div className='h-32 bg-button-primary overflow-hidden'>
-              <img src={lgCat} className='h-full w-full mt-10' alt='too many cats' />
+              <img
+                src={lgCat}
+                className='h-full w-full mt-10'
+                alt='too many cats'
+              />
             </div>
             <div className='text-center rounded-lg bg-primary p-4 text-button-primary'>
-              <div className='text-2xl font-heading'>Large</div>
-              <div>Articles per Month - 4</div>
-              <div>Articles per Week - 1</div>
-              <div>Words per article - 300</div>
-              <div>SEO Keyword Writing - Yes</div>
-              <div>Content Stategy Included - Yes</div>
-              <div>Plagarism Free - Yes</div>
-              <div>Posting Included - No</div>
-              <div className='text-pricing-content-bold'>$39.00 / month</div>
-              <button className='bg-button-primary hover:bg-gray-700 text-primary  font-bold py-2 px-4 mt-4 rounded-full'>Buy Now</button>
+              <div className='text-2xl font-heading'>Enterprise</div>
+              <div>6 Pillar Pages (4000 Words Each)</div>
+              <div>14 SEO Articles (2000 Words Each)</div>
+              <div>20 Custom Banners</div>
+              <div>Customized Content Strategy </div>
+              <div>Dedicated Account Manager</div>
+              <div className='text-pricing-content-bold'>$5000 / month</div>
+              <button className='bg-button-primary hover:bg-gray-700 text-primary  font-bold py-2 px-4 mt-4 rounded-full'>
+                Buy Now
+              </button>
             </div>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Pricing;
+export default Pricing
